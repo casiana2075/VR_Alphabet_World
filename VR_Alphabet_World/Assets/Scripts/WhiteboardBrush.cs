4d61a2cd10fc2398993c66d32155b1915c540540
Casiana <88460309+casiana2075@users.noreply.github.com>
2024-12-18 23:04:33 +0200

    created letter guidelines for whiteboard and also the grab for brush+pen

--- VR_Alphabet_World/Assets/Scripts/WhiteboardBrush.cs
+++ VR_Alphabet_World/Assets/Scripts/WhiteboardBrush.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using Unity.Collections;
 using UnityEngine;
 
-public class WhiteboardPen : MonoBehaviour
+public class WhiteboardBrush : MonoBehaviour
 {
     [SerializeField] private Transform _tip;
-    [SerializeField] private int _penSize = 30;
+    [SerializeField] private int _brushSize = 30;
 
     private Renderer _renderer;
     private Color[] _colors;
@@ -22,7 +22,7 @@ public class WhiteboardPen : MonoBehaviour
     void Start()
     {
         _renderer = _tip.GetComponent<Renderer>();
-        _colors = Enumerable.Repeat(_renderer.material.color, _penSize * _penSize).ToArray();
+        _colors = Enumerable.Repeat(_renderer.material.color, _brushSize * _brushSize).ToArray();
         _tipHeight = _tip.localScale.y;
     }
 
@@ -44,20 +44,20 @@ public class WhiteboardPen : MonoBehaviour
 
                 _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
 
-                var x = (int)(_touchPos.x * _whiteboard.textureSize.x - (_penSize / 2));
-                var y = (int)(_touchPos.y * _whiteboard.textureSize.y - (_penSize / 2));
+                var x = (int)(_touchPos.x * _whiteboard.textureSize.x - (_brushSize / 2));
+                var y = (int)(_touchPos.y * _whiteboard.textureSize.y - (_brushSize / 2));
 
                 if (y < 0 || y > _whiteboard.textureSize.y || x < 0 || x > _whiteboard.textureSize.x) return;
 
                 if (_touchedLastFrame)
                 {
-                    _whiteboard.texture.SetPixels(x, y, _penSize, _penSize, _colors);
+                    _whiteboard.texture.SetPixels(x, y, _brushSize, _brushSize, _colors);
 
                     for (float f = 0.01f; f < 1.00f; f += 0.01f)
                     {
                         var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
                         var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
-                        _whiteboard.texture.SetPixels(lerpX, lerpY, _penSize, _penSize, _colors);
+                        _whiteboard.texture.SetPixels(lerpX, lerpY, _brushSize, _brushSize, _colors);
                     }
 
                     transform.rotation = _lastTouchRot;
